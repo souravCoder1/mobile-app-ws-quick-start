ebd9193bd3d521c756ede9334aa268ddf6a5e2ae
souravpal01 <souravpal7217@gmail.com>
2023-02-28 22:25:00 +0530

    REST app

---
+++ src/main/java/com/appsdeveloperblog/app/ws/exceptions/UserServiceException.java
@@ -0,0 +1,15 @@
+package com.appsdeveloperblog.app.ws.exceptions;
+
+public class UserServiceException extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8334433275461677590L;
+
+	public UserServiceException(String message)
+	{
+		super(message);
+	}
+
+}
